27c07e4e0ad15f2a60f6c569d1f68f33372164f5
Sucharita Lahiri <34596224+sucharitalahiri@users.noreply.github.com>
2026-07-16 13:27:41 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://hotel-booking-app-gffx.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
