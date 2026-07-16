a7d4c0128af5ebdd53c5e36f304e8bb0ce6adfa7
Sucharita Lahiri <34596224+sucharitalahiri@users.noreply.github.com>
2026-07-16 12:30:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,9 +32,9 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
-
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
   })
   .catch((err) => console.log(err));
